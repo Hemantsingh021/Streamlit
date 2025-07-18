5298e15c71676d1417a7ad8e33a4760b38cd6d2c
Hemantsingh021 <147120678+Hemantsingh021@users.noreply.github.com>
2025-07-18 09:56:20 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pickle
+
+with open('knn.pkl','rb') as f:
+    model = pickle.load(f)
+
+age=st.number_input("Enter Age")
+salary=st.number_input("Salary")
+exp=st.number_input("Experience (Years)")
+dept=st.number_input("Enter Department Number")
+
+pred=model.predict([[age,salary,exp,dept]])
+if st.button('Analyse'):
+    if pred==1:
+        st.success("You are doing good")
+    else:
+        st.warning("Chances of layoff")
+
